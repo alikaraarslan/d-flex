306d988a552af8f27be6ea7c509463958a19cd19
Ali Karaarslan <ali.karaarslan@bilgeadam.com>
2022-05-25 22:05:22 +0300

    add loading animate

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
+import { useEffect, useState } from 'react';
+import Loader from '../components/Loader';
 import StyledThemeProvider from '../styles/ThemeProvider';
 function MyApp({ Component, pageProps }: any) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 500);
+  }, []);
   return (
     <StyledThemeProvider>
       <Component {...pageProps} />
+      {loading && <Loader />}
     </StyledThemeProvider>
   );
 }
